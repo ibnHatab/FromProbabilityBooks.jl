1ea18e4384424c0788112a9f871937f5a773eb0f
Documenter.jl <documenter@juliadocs.github.io>
2022-09-11 09:46:15 +0000

    build based on 72fa1cc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FromProbabilityBooks","category":"page"},{"location":"#FromProbabilityBooks","page":"Home","title":"FromProbabilityBooks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FromProbabilityBooks.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FromProbabilityBooks]","category":"page"}]
+}
